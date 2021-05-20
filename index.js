3ba74e7ede63d17edb6787e2bdf2a2d74ac8d430
lycho33 <cho.lyds@gmail.com>
2021-05-20 15:35:15 -0400

    commit tree 8f8f042d77c132b23d04bef946a2ddd6b1ff27ac

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let words = string.split( " " )
+    for (let i=0; i < words.length; i++) {
+      if (i = 0){
+        result.push(this.capitalize(words[0]))
+      } 
+      return console.log(result)
+    }
+  }
 }
